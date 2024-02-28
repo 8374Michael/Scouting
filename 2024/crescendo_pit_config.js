ac4488bd40ca0c944a8bec1301c772f2505ab015
FRC8374 <160941546+8374Michael@users.noreply.github.com>
2024-02-28 16:30:42 -0500

    Fix T

--- 2024/crescendo_pit_config.js
+++ 2024/crescendo_pit_config.js
@@ -6,7 +6,7 @@ var config_data = `
   "pitConfig": "true",
   "prematch": [
     { "name": "Team Number",
-      "code": "tn",
+      "code": "t",
       "type": "number"
     },
     { "name": "Width",
