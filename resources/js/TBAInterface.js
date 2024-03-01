bfe77ee12997cde8613de8ee348665574518411b
FRC8374 <160941546+8374Michael@users.noreply.github.com>
2024-03-01 03:21:29 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "zn8P2P0pXQ0gLwPMZwoJYBlAhut0iSWBgqUJYhiHovtdCKWPpG7SbRrPtN7tLuif";
 /**
  * Get list of teams in event
  *
